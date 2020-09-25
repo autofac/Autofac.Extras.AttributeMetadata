15ede5aa60d2ec478957315031083b3eb65e251b
alexmg <alex.meyergleaves@gmail.com>
2020-09-26 00:24:06 +1000

    Compatibility with Autofac v6

--- src/Autofac.Extras.AttributeMetadata/AutofacAttributeExtensions.cs
+++ src/Autofac.Extras.AttributeMetadata/AutofacAttributeExtensions.cs
@@ -1,27 +1,5 @@
-﻿// This software is part of the Autofac IoC container
-// Copyright © 2013 Autofac Contributors
-// https://autofac.org
-//
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
 using System.Linq;
@@ -52,7 +30,10 @@ namespace Autofac.Extras.AttributeMetadata
             WithAttributedMetadata<TLimit, TScanningActivatorData, TRegistrationStyle>(this IRegistrationBuilder<TLimit, TScanningActivatorData, TRegistrationStyle> builder)
                 where TScanningActivatorData : ScanningActivatorData
         {
-            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
 
             builder.ActivatorData.ConfigurationActions.Add(
                 (t, rb) => rb.WithMetadata(MetadataHelper.GetMetadata(t)));
@@ -74,7 +55,10 @@ namespace Autofac.Extras.AttributeMetadata
         public static IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle>
             WithAttributedMetadata<TMetadata>(this IRegistrationBuilder<object, ScanningActivatorData, DynamicRegistrationStyle> builder)
         {
-            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
 
             builder.ActivatorData.ConfigurationActions.Add(
                 (t, rb) => rb.WithMetadata(MetadataHelper.GetMetadata<TMetadata>(t)));
@@ -111,7 +95,10 @@ namespace Autofac.Extras.AttributeMetadata
                 this IRegistrationBuilder<TLimit, TReflectionActivatorData, TRegistrationStyle> builder)
             where TReflectionActivatorData : ReflectionActivatorData
         {
-            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
 
 #pragma warning disable CS0618
             return builder.WithParameter(
--- src/Autofac.Extras.AttributeMetadata/MetadataHelper.cs
+++ src/Autofac.Extras.AttributeMetadata/MetadataHelper.cs
@@ -1,27 +1,5 @@
-﻿// This software is part of the Autofac IoC container
-// Copyright (c) 2007 - 2013 Autofac Contributors
-// https://autofac.org
-//
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
 using System.Collections.Generic;
@@ -45,13 +23,20 @@ namespace Autofac.Extras.AttributeMetadata
         /// <see cref="IMetadataProvider"/>.
         /// </param>
         /// <returns>Enumerable set of property names and associated values.</returns>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         /// Thrown if <paramref name="target" /> or <paramref name="instanceType"/> is <see langword="null" />.
         /// </exception>
         public static IEnumerable<KeyValuePair<string, object>> GetProperties(object target, Type instanceType)
         {
-            if (target == null) throw new ArgumentNullException(nameof(target));
-            if (instanceType == null) throw new ArgumentNullException(nameof(instanceType));
+            if (target == null)
+            {
+                throw new ArgumentNullException(nameof(target));
+            }
+
+            if (instanceType == null)
+            {
+                throw new ArgumentNullException(nameof(instanceType));
+            }
 
             if (target is IMetadataProvider asProvider)
             {
@@ -73,12 +58,15 @@ namespace Autofac.Extras.AttributeMetadata
         /// </summary>
         /// <param name="targetType">Type to interrogate for metadata attributes.</param>
         /// <returns>Enumerable set of property names and associated values found.</returns>
-        /// <exception cref="System.ArgumentNullException">
+        /// <exception cref="ArgumentNullException">
         /// Thrown if <paramref name="targetType" /> is <see langword="null" />.
         /// </exception>
         public static IEnumerable<KeyValuePair<string, object>> GetMetadata(Type targetType)
         {
-            if (targetType == null) throw new ArgumentNullException(nameof(targetType));
+            if (targetType == null)
+            {
+                throw new ArgumentNullException(nameof(targetType));
+            }
 
             var propertyList = new List<KeyValuePair<string, object>>();
 
@@ -104,7 +92,10 @@ namespace Autofac.Extras.AttributeMetadata
         /// </exception>
         public static IEnumerable<KeyValuePair<string, object>> GetMetadata<TMetadataType>(Type targetType)
         {
-            if (targetType == null) throw new ArgumentNullException(nameof(targetType));
+            if (targetType == null)
+            {
+                throw new ArgumentNullException(nameof(targetType));
+            }
 
             var attribute = (from p in targetType.GetCustomAttributes(typeof(TMetadataType), true) select p).FirstOrDefault();
 
